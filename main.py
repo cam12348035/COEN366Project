cce38acd498241851dd337b5a749970229f52dac
cam12348035 <camgranade@gmail.com>
2025-11-24 11:10:13 -0500

    Remade thread system to respond to requirements

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#TODO:
+#Install mutex/lock on at least udp_request_list and request_number_list
+
+
 import socket
 from registration import registration_handler, deregistration_handler
 import sys
@@ -10,6 +14,9 @@ TCP_PORT = 6000
 MAX_CLIENTS = 50
 
 peer_list = {}
+udp_request_list = []
+request_number_list = []
+
 
 # UDP socket
 try:
@@ -31,66 +38,94 @@ except:
     print('Failed to create TCP. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
+def registration_handler(split_message, addr, udp_sock):
+    if len(split_message) < 8:
+        print("Message incorrect format")
+        return
+    rq_num = split_message[1]
+    name = split_message[2]
+    role = split_message[3]
+    ip_add = split_message[4]
+    udp_port = split_message[5]
+    tcp_port = split_message[6]
+    storage = split_message[7]
+    
+    if name in peer_list:
+        response = f"REGISTER-DENIED {rq_num} Name already in use"
+        udp_sock.sendto(response.encode(), addr)
+        print(f"Registration denied {addr}, {name}: {response}")
+        return
+    
+    if len(peer_list) >= MAX_CLIENTS:
+        response = f"REGISTER-DENIED {rq_num} Server cannot handle additional clients"
+        udp_sock.sendto(response.encode(), addr)
+        print(f"Registration denied {addr}, {name}: {response}")
+        return
+    
+    #Register peer
+    peer_list[name] = (role, ip_add, udp_port, tcp_port, storage)
+    print(f"Registered: {name}, {role}")
+    
+    response = f"REGISTERED {rq_num}"
+    udp_sock.sendto(response.encode(), addr)
+    print(f"Response sent to {addr}: {response}")
+    
+    request_number_list.remove(rq_num)
 
-def tcp_client(client_sock, addr):
-    print(f"TCP from {addr}")
-    try:
-        while True:
-            data = client_sock.recv(4096)
-            if not data:
-                break
-            
-            message = data.decode()
-            print(f"TCP received from {addr}: {message}")
-            
-#Gotta put in TCP requests
-            response = message
-            client_sock.send(response.encode())
-            
-    finally:
-        client_sock.close()
-        print(f"TCP connection closed: {addr}")
+def deregistration_handler(split_message, addr, udp_sock):
+    rq_num = split_message[1]
+    name = split_message[2]
+    
+    if name in peer_list:
+        del peer_list[name]
+        print(f"De-registered: {name}")
+        response = f"DE-REGISTERED {rq_num}"
+        udp_sock.sendto(response.encode(), addr)
+        print(f"Sent to {addr}: {response}")
+    else:
+        print(f"De-register ignored: {name} not found")
+    request_number_list.remove(rq_num)
 
-def udp_server():
+
+
+
+def main_thread():
     try:
         while True:
             data, addr = udp_sock.recvfrom(4096)
             message = data.decode()
-            print(f"UDP received from {addr}: {message}")
-            
-            split_message  = message.split()
-            command = split_message [0]
-            
-            if command == "REGISTER":
-                registration_handler(split_message, addr, peer_list, MAX_CLIENTS, udp_sock)
-            elif command == "DE-REGISTER":
-                deregistration_handler(split_message, addr, peer_list, udp_sock)
+            split_message = message.split()
+            if len(split_message) < 3:
+                print("Wrong message size")
+                continue
+
+            if split_message[1] in request_number_list:
+                udp_request_list.append([message,addr])
+            else:
+                request_number_list.append(split_message[1])
+                if split_message[0] == "REGISTER":
+                    subthread = threading.Thread(target=registration_handler, args =(split_message, addr, udp_sock))
+
+                elif split_message[0] == "DE-REGISTER":
+                    subthread = threading.Thread(target=deregistration_handler, args = (split_message, addr, udp_sock))
+                    
+                subthread.daemon = True
+                subthread.start()
     except KeyboardInterrupt:
         sys.exit()
 
-def tcp_server():
-    try:
-        while True:
-            client_sock, addr = tcp_sock.accept()
-            # Each TCP peer has its own thread
-            thread = threading.Thread(target=tcp_client, args=(client_sock, addr))
-            thread.daemon = True
-            thread.start()
-    except KeyboardInterrupt:
-        sys.exit()
+
+
+            
 
 
 
 #Main code
-# UDP thread
-udp_thread = threading.Thread(target=udp_server)
-udp_thread.daemon = True
-udp_thread.start()
+# Main thread 
+main_th = threading.Thread(target=main_thread)
+main_th.daemon = True
+main_th.start()
 
-# TCP Thread
-tcp_thread = threading.Thread(target=tcp_server)
-tcp_thread.daemon = True
-tcp_thread.start()
 try:
     while True:
         pass
